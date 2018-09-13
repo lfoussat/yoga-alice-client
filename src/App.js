09711db80b57e81fb2cda52c11bfcfc55fec207d
Louise Foussat <louisefoussat@gmail.com>
2018-09-13 19:03:55 +0200

    refactor app.js with reach router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
+import { Router } from '@reach/router'
+import Home from './containers/Home.js'
+import InspirationsDisplay from './containers/InspirationsDisplay.js'
+import InspirationDetail from './containers/InspirationDetail.js'
+import YogaAlice from './containers/YogaAlice.js'
+import Courses from './containers/Courses.js'
+import Shiatsu from './containers/Shiatsu.js'
+import Stages from './containers/Stages.js'
+import Contact from './containers/Contact.js'
+import { getAllInspirations } from './api.js'
+
 import './App.css'
 
 class App extends Component {
+  state = {
+    inspirations: [],
+    inspiration: []
+  }
+
+  constructor() {
+    super()
+    getAllInspirations().then(i => this.setState({ inspirations: i }))
+  }
+
   render () {
+    console.log(this.state.inspirations)
+    console.log(this.state.inspiration)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Home {...this.state} path='/' />
+        <YogaAlice path='/yoga-alice' />
+        <Courses {...this.state} path='/cours' />
+        <Shiatsu path='/shiatsu' />
+        <Stages path='/stages' />
+        <InspirationsDisplay {...this.state} path='/inspirations' />
+        <InspirationDetail {...this.state} path='/inspirations/:id' />
+        <Contact path='/contact' />
+      </Router>
     )
   }
 }
