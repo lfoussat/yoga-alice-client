29f6cedbbbf51ff1185351ed9cf9f0bbe8ee2a22
Louise Foussat <louisefoussat@gmail.com>
2018-09-13 19:00:42 +0200

    create carousel component

---
+++ src/components/Carousel.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Carousel.css'
+
+const Carousel = ({slides, currentSlide}) =>
+  <div id="yoga_alice_carousel">
+    <img className="mySlides ya-animate-fading" src={slides[currentSlide]} alt='carousel' />
+  </div>
+
+
+export default Carousel
