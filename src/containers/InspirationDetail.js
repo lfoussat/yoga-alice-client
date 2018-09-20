fc509a7df4417c6d95ebbc15f9696f71e6d23fad
Louise Foussat <louisefoussat@gmail.com>
2018-09-20 10:49:14 +0200

    refactor inspiration detail according to the store -  redux

--- src/containers/InspirationDetail.js
+++ src/containers/InspirationDetail.js
@@ -2,36 +2,39 @@ import React, { Component } from 'react'
 import Header from '../components/Header.js'
 import Footer from '../components/Footer.js'
 import { Container, Grid } from 'semantic-ui-react'
-import { getInspirationById } from '../api.js'
+import { api, getInspirationById } from '../api.js'
+import { actions } from '../store.js'
 import './InspirationDetail.css'
 
 class InspirationDetail extends Component {
-  state = {
-    inspiration: {}
-  }
-
   componentDidMount () {
-    getInspirationById(this.props.id).then(i => this.setState({ inspiration: i }))
+    getInspirationById(this.props.id, 'fo')
+      .then(actions.loadInspiration)
   }
 
   render () {
-    console.log(this.state.inspiration)
     return (
       <Container fluid>
         <Header />
-        <h1>{this.state.inspiration.title}</h1>
-        <h2>{this.state.inspiration.smallDescription}</h2>
+        <h1>{this.props.inspiration.title}</h1>
+        <h2>{this.props.inspiration.smallDescription}</h2>
         <Grid>
           <Grid.Column
             mobile={16}
             tablet={16}
             computer={6}
             className="pPicture">
-            <img alt={`${this.state.inspiration.title}`} src={`${this.state.inspiration.imageUrl}`}/></Grid.Column>
+            <img
+              alt={`${this.props.inspiration.title}`}
+              src={`${api}/images/inspirations/${this.props.inspiration.imageUrl}`}/>
+          </Grid.Column>
           <Grid.Column
             mobile={16}
             tablet={16}
-            computer={10} className="pText"><p>{this.state.inspiration.description}</p></Grid.Column>
+            computer={10}
+            className="pText">
+            <p>{this.props.inspiration.description}</p>
+          </Grid.Column>
         </Grid>
         <Footer />
       </Container>
