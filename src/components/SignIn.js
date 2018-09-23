5fde111e51eca21516d3dc5dd8e6bbd7e1e6dc39
Louise Foussat <louisefoussat@gmail.com>
2018-09-23 17:18:56 +0200

    create sign in js

---
+++ src/components/SignIn.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from '@reach/router'
+import AuthForm from './AuthForm.js'
+import AuthHeader from '../components/AuthHeader'
+//import './styleError.css'
+
+const SignIn = ({ onSubmit, errors }) => (
+  <React.Fragment>
+    <AuthHeader />
+    <AuthForm
+      onSubmit={onSubmit}
+      headerMessage='Log-in to your account'
+      actionName='Login'>
+      New to us? <Link to='/sign-up'>Sign Up</Link>
+    </AuthForm>
+    { !errors.login ? undefined : <div className='ui message error centerError'>{errors.login}</div> }
+  </React.Fragment>
+)
+
+export default SignIn
