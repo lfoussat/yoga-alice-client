45d514d82ec964036572c907e17a9edf8581514b
Louise Foussat <louisefoussat@gmail.com>
2018-09-24 08:56:54 +0200

    clean css

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -3,28 +3,32 @@ import { Button, Form, Grid, Segment, Message } from 'semantic-ui-react'
 
 const AuthForm = ({ headerMessage, actionName, onSubmit, error, children }) => (
   <div className='AuthForm'>
-    <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
+    <Grid centered style={{ height: '100%' }} verticalAlign='middle'>
       <Grid.Column style={{ maxWidth: 450 }}>
-        <h2 style={{ marginTop: '125px' }}>
+        <h1>
           {headerMessage}
-        </h2>
+        </h1>
         <Form size='large' onSubmit={onSubmit}>
-          <Segment stacked>
-            <Form.Input name='email' type='email' fluid icon='user' iconPosition='left' placeholder='E-mail address' />
-            <Form.Input
-              fluid
-              name='password'
-              icon='lock'
-              iconPosition='left'
-              placeholder='Password'
-              type='password'
-              minLength='6'
-              maxLength='18'
-            />
-            <Button fluid size='large'>
-              {actionName}
-            </Button>
-          </Segment>
+          <Form.Input
+            name='email'
+            type='email'
+            fluid
+            icon='user'
+            iconPosition='left'
+            placeholder='E-mail address' />
+          <Form.Input
+            fluid
+            name='password'
+            icon='lock'
+            iconPosition='left'
+            placeholder='Password'
+            type='password'
+            minLength='6'
+            maxLength='18'
+          />
+          <Button fluid size='large'>
+            {actionName}
+          </Button>
         </Form>
         <Message negative={Boolean(error)}>
           {error || children}
