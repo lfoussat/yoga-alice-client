806b40834a8947d1233f711f8871e459612ed572
Louise Foussat <louisefoussat@gmail.com>
2018-10-08 17:30:28 +0200

    update carousel component according to the loading data from db

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import './Carousel.css'
+import { api } from '../api.js'
 
 const Carousel = ({ slides, currentSlide }) =>
   <div id="yoga_alice_carousel">
-    <img className="mySlides ya-animate-fading" src={slides[currentSlide]} alt='carousel' />
+    <img className="mySlides ya-animate-fading" src={`${api}/images/${slides[currentSlide].imageUrl}`} alt={slides[currentSlide].title} />
   </div>
 
 export default Carousel
