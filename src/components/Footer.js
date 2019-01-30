5dc7f4ad0c0fb6f82a38a430714ce53642ce7d69
Louise Foussat <louisefoussat@gmail.com>
2019-01-30 18:24:07 +0100

    Enhance footer -> add instagram link & mention of web dev

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,20 +3,33 @@ import { Link } from '@reach/router'
 import { Icon } from 'semantic-ui-react'
 import './Footer.css'
 
-const Footer = () =>
+const Footer = () => (
   <section>
     <div id="yogaAliceFooter">
       <div id="socialMedia">
-        <a href="https://www.facebook.com/alice.yoga.shiatsu/"><Icon name='facebook f' />Yoga Alice sur Facebook</a>
-        <Link to="/contact"><Icon name='mail' />Contact</Link>
+        <a href="https://www.facebook.com/alice.yoga.shiatsu/" target="_blank">
+          <Icon name="facebook f" />
+          Yoga Alice sur Facebook
+        </a>
+        <a href="https://www.instagram.com/aliceollagnon/" target="_blank">
+          <Icon name="instagram" />
+          aliceollagnon sur Instagram
+        </a>
+        <Link to="/contact">
+          <Icon name="mail" />
+          Contact
+        </Link>
       </div>
       <div id="footerLinks">
         <Link to="/site-map">Plan du site</Link>
       </div>
       <div id="coyright">
-        <p>Copyright 2018 : Alice Olagnon</p>
+        <p>
+          Copyright 2019 : Alice Olagnon - Développement web : Louise Foussat
+        </p>
       </div>
     </div>
   </section>
+)
 
 export default Footer
