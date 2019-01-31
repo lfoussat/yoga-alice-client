9e32bf391b4a25330e64615b029c9bb8a6dfa574
Louise Foussat <louisefoussat@gmail.com>
2019-01-31 13:16:10 +0100

    update api

--- src/api.js
+++ src/api.js
@@ -1,41 +1,41 @@
 /* global fetch */
 
-export const api = 'http://localhost:5300'
+export const api = 'https://alice-ollagnon-server.herokuapp.com'
 
-const postJson = (url, content) => fetch(url, {
-  method: 'post',
-  headers: {
-    'X-Access-Token': localStorage.token,
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(content)
-})
+const postJson = (url, content) =>
+  fetch(url, {
+    method: 'post',
+    headers: {
+      'X-Access-Token': localStorage.token,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(content)
+  })
 
 // INSPIRATIONS
-export const getAllInspirations = () => { // get all inspirations
-  return fetch(`${api}/fo/inspirations`)
-    .then(res => res.json())
+export const getAllInspirations = () => {
+  // get all inspirations
+  return fetch(`${api}/fo/inspirations`).then(res => res.json())
 }
 
 export const getAllInspirationsForBO = () => {
-  return fetch(`${api}/bo/inspirations`,
-    {
-      headers: { 'X-Access-Token': localStorage.token }
-    })
-    .then(res => res.json())
+  return fetch(`${api}/bo/inspirations`, {
+    headers: { 'X-Access-Token': localStorage.token }
+  }).then(res => res.json())
 }
 
-export const getInspirationById = (id, side) => { // get an inspiration
-  return fetch(`${api}/${side}/inspirations/${id}`)
-    .then(res => res.json())
+export const getInspirationById = (id, side) => {
+  // get an inspiration
+  return fetch(`${api}/${side}/inspirations/${id}`).then(res => res.json())
 }
 
-export const sendInspirationDb = title => { // send a new inspiration
-  return postJson(`${api}/inspirations`, { title })
-    .then(res => res.json())
+export const sendInspirationDb = title => {
+  // send a new inspiration
+  return postJson(`${api}/inspirations`, { title }).then(res => res.json())
 }
 
-export const updateInspiration = (id, formData) => { // update an inspiration
+export const updateInspiration = (id, formData) => {
+  // update an inspiration
   console.log('coucou - ', id, formData)
   return fetch(`${api}/inspirations/${id}`, {
     method: 'post',
@@ -43,25 +43,22 @@ export const updateInspiration = (id, formData) => { // update an inspiration
       'X-Access-Token': localStorage.token
     },
     body: formData
-  })
-    .then(res => res.json())
+  }).then(res => res.json())
 }
 
-export const deleteInspirationDb = params => { // delete an inspiration
+export const deleteInspirationDb = params => {
+  // delete an inspiration
   return fetch(`${api}/inspirations/${params.id}`, {
     method: 'delete',
     headers: {
       'X-Access-Token': localStorage.token
     }
-  })
-    .then(res => res.json())
+  }).then(res => res.json())
 }
 
 // AUTHENTICATION
-export const sendSignUp = (params) =>
-  postJson(`${api}/users`, params)
-    .then(res => res.json())
+export const sendSignUp = params =>
+  postJson(`${api}/users`, params).then(res => res.json())
 
-export const sendLogin = (params) =>
-  postJson(`${api}/auth/local`, params)
-    .then(res => res.json())
+export const sendLogin = params =>
+  postJson(`${api}/auth/local`, params).then(res => res.json())
